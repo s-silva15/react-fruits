3928da6e1aeaa646a9c663f6b1a49307e8ef5fc3
stanley <stanleypmsilva@gmail.com>
2023-07-15 22:05:48 -0300

    component redbutton

---
+++ src/components/redButton/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+function RedButton() {
+  return (
+    <div style={style.containerStyle}>
+      <span style={style.spanStyle}>Cadastre sua primeira fruta.</span>
+      <button style={style.buttonStyle}>
+        <FontAwesomeIcon icon={faPlus} style={style.iconStyle} />
+        Cadastrar fruta
+      </button>
+    </div>
+  );
+}
+
+export default RedButton;
+
+const style = {
+  spanStyle: {
+    color: "black",
+    fontSize: "16px",
+    marginBottom: '10px',
+  },
+  buttonStyle: {
+    fontSize: "16px",
+    display: 'flex',
+    width: '328px',
+    height: '40px',
+    padding: '8px 24px 8px 16px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '8px',
+    flexShrink: '0',
+    borderRadius: '100px',
+    backgroundColor: 'red',
+    color: 'white',
+    border: 'none'
+  },
+  iconStyle: {
+    marginRight: '5px',
+  },
+  containerStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: 'white',
+  }
+};
+
